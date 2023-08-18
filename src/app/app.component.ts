bf84d8b6129f1a91331b359bf498338f9f476fd7
Andrew Wallace <wall0419@algonquinlive.com>
2023-08-18 12:22:02 -0400

    project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-second-app';
+  title = 'My App';
+  hello = 'hello';
+  name = 'Andrew';
 }
