aab2bbc4ce235311aefe845cd676d75eca36a6be
Andrew Wallace <wall0419@algonquinlive.com>
2023-08-18 18:36:20 -0400

    working on a3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,17 @@ export class AppComponent {
   title = 'My App';
   hello = 'hello';
   name = 'Andrew';
+  showDetails: boolean = false;
+  buttonClicks = [];
+  buttonClick = 0;
 
   randomString(): string{
     return "Hellow World";
+  }
+
+  onShowDetails(){
+    this.showDetails = true;
+    this.buttonClick++;
+    this.buttonClicks.push(this.buttonClick);
   }
 }
