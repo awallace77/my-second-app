9da3a2c0c72a31734da209cebb99010af52546fd
Andrew Wallace <wall0419@algonquinlive.com>
2023-08-25 16:50:25 -0400

    practicing debugging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,17 @@ export class AppComponent {
   title = 'My App';
   hello = 'hello';
   name = 'Andrew';
+  server = [];
 
   randomString(): string{
     return "Hellow World";
+  }
+
+  onAddServer(){
+    this.server.push('Another Server');
+  }
+
+  onRemoveServer(id: number){
+    this.server.splice(id, 1)
   }
 }
